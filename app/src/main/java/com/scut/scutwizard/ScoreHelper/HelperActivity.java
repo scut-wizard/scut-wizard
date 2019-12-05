8c85c19719bd6dbbaa719aacd45d25aaedba202d
MinutesSneezer <546241245@qq.com>
2019-12-06 01:44:11 +0800

    Fix: Tab touched

--- app/src/main/java/com/scut/scutwizard/ScoreHelper/HelperActivity.java
+++ app/src/main/java/com/scut/scutwizard/ScoreHelper/HelperActivity.java
@@ -2,6 +2,7 @@ package com.scut.scutwizard.ScoreHelper;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
@@ -21,7 +22,10 @@ public class HelperActivity extends AppCompatActivity implements StatsFragment.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helper);
         setTitle(R.string.helper);
+        initView();
+    }
 
+    private void initView() {
         tabLayout = findViewById(R.id.helper_tab_layout);
         vp = findViewById(R.id.helper_vp);
 
@@ -32,6 +36,21 @@ public class HelperActivity extends AppCompatActivity implements StatsFragment.O
 
         vp.setAdapter(adapter);
         tabLayout.setupWithViewPager(vp);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("TAG", "" + tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
     @Override
